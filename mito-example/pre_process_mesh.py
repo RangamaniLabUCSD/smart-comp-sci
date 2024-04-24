6bb3d3ca978977c2a86a97230bfab56f20b8b52c
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-04-24 09:36:47 +0200

    Clean up mito example

--- mito-example/pre_process_mesh.py
+++ mito-example/pre_process_mesh.py
@@ -6,36 +6,44 @@ from smart import mesh_tools
 import mito_parser_args
 import numpy as np
 
-def main(input_mesh_file, output_mesh_file, input_curv_file, 
-         output_curv_file, num_refinements, single_compartment_im=False):
+
+def main(
+    input_mesh_file,
+    output_mesh_file,
+    input_curv_file,
+    output_curv_file,
+    num_refinements,
+    single_compartment_im=False,
+):
     if not Path(input_mesh_file).is_file():
         raise FileNotFoundError(f"File {input_mesh_file} does not exist")
-    
 
     mito_mesh = d.Mesh(Path(input_mesh_file).as_posix())
     cell_markers = d.MeshFunction("size_t", mito_mesh, 3, mito_mesh.domains())
     facet_markers = d.MeshFunction("size_t", mito_mesh, 2, mito_mesh.domains())
 
     if single_compartment_im:
-        facet_markers.array()[np.where(facet_markers.array()==11)[0]] = 12
-        print(f"Remove cristae markers, single compartment im is {single_compartment_im}")
+        facet_markers.array()[np.where(facet_markers.array() == 11)[0]] = 12
+        print(
+            f"Remove cristae markers, single compartment im is {single_compartment_im}"
+        )
 
-    # if num_refinements > 0:
-    #     print(
-    #         f"Original mesh has {mito_mesh.num_cells()} cells, "
-    #         f"{mito_mesh.num_facets()} facets and "
-    #         f"{mito_mesh.num_vertices()} vertices"
-    #     )
-    #     d.parameters["refinement_algorithm"] = "plaza_with_parent_facets"
-    #     for _ in range(num_refinements):
-    #         mito_mesh = d.adapt(mito_mesh)
-    #         cell_markers = d.adapt(cell_markers, mito_mesh)
-    #         facet_markers = d.adapt(facet_markers, mito_mesh)
-    #     print(
-    #         f"Refined mesh has {mito_mesh.num_cells()} cells, "
-    #         f"{mito_mesh.num_facets()} facets and "
-    #         f"{mito_mesh.num_vertices()} vertices"
-    #     )
+    if num_refinements > 0:
+        print(
+            f"Original mesh has {mito_mesh.num_cells()} cells, "
+            f"{mito_mesh.num_facets()} facets and "
+            f"{mito_mesh.num_vertices()} vertices"
+        )
+        d.parameters["refinement_algorithm"] = "plaza_with_parent_facets"
+        for _ in range(num_refinements):
+            mito_mesh = d.adapt(mito_mesh)
+            cell_markers = d.adapt(cell_markers, mito_mesh)
+            facet_markers = d.adapt(facet_markers, mito_mesh)
+        print(
+            f"Refined mesh has {mito_mesh.num_cells()} cells, "
+            f"{mito_mesh.num_facets()} facets and "
+            f"{mito_mesh.num_vertices()} vertices"
+        )
 
     Path(output_mesh_file).parent.mkdir(exist_ok=True, parents=True)
     mesh_tools.write_mesh(mito_mesh, facet_markers, cell_markers, output_mesh_file)
--- scripts/run_dendtritic_spine.sh
+++ scripts/run_dendtritic_spine.sh
@@ -6,16 +6,23 @@
 # python3 main.py dendritic-spine-preprocess --input-mesh-file meshes-dendritic-spine/2spine_PM10_PSD11_ERM12_cyto1_ER2.xml --output-mesh-file meshes-dendritic-spine/2spine_mesh.h5 --num-refinements 0
 partition=defq
 # # # Base case
-for timestep in 0.01 0.001 0.001 0.0001
-do
-    python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
-    sleep 5
-    python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
-    sleep 5
-    python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
-    sleep 5
-    python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh.h5 --time-step $timestep -o results-dendritic-spine/1spine_timestep_${timestep}
-    sleep 5
-    python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/2spine_mesh.h5 --time-step $timestep -o results-dendritic-spine/2spine_timestep_${timestep}
-    sleep 5
-done
+# for timestep in 0.01 0.001 0.001 0.0001
+# do
+#     python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
+#     sleep 5
+#     python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
+#     sleep 5
+#     python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step $timestep -o results-dendritic-spine/1spine_coarser_timestep_${timestep}
+#     sleep 5
+#     python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh.h5 --time-step $timestep -o results-dendritic-spine/1spine_timestep_${timestep}
+#     sleep 5
+#     python3 main.py --submit-ex3 --partition=$partition dendritic-spine --mesh-file meshes-dendritic-spine/2spine_mesh.h5 --time-step $timestep -o results-dendritic-spine/2spine_timestep_${timestep}
+#     sleep 5
+# done
+
+
+# python3 main.py dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser.h5 --time-step  0.001 -o results-dendritic-spine/coarser
+# sleep 5
+# python3 main.py dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser_refined_1.h5 --time-step  0.001 -o results-dendritic-spine/coarser_refined_1
+# sleep 5
+python3 main.py dendritic-spine --mesh-file meshes-dendritic-spine/1spine_mesh_coarser_refined_2.h5 --time-step  0.001 -o results-dendritic-spine/coarser_refined_2
